2064879567746029c751b3471fe0a3a729e11f2f
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2023-06-23 22:09:02 -0300

    Gerador Incompleto
    Gerador parcialmente funcionando.

--- js/gerador_de_salas.js
+++ js/gerador_de_salas.js
@@ -7,16 +7,47 @@ var $cadastrar_sala_btn = document.getElementById('cadastrar_sala_btn')
 
 $cadastrar_sala_btn.addEventListener('click', () => {
 
-    var hora_inicial = 'Data/hora inicio: '
-    var hora_final = 'Data/hora fim: '
-    var novaSala = document.createElement('h1')
-    var codigo = document.createTextNode("Sala " + $texto_codigo_sala.value)
-    if (novaSala.classList) {
-        novaSala.classList.add("sala");
+    //Variaveis que criam elementos do HTML.
+    var nova_sala = document.createElement('div')
+    var h4_nova_sala = document.createElement('h4')
+    var responsavel = document.createElement('p')
+    var data_inicio = document.createElement('p')
+    var data_final = document.createElement('p')
+
+    //Variaveis que receberão os dados do formulário de registro das salas.
+    var codigo_sala = document.createTextNode("Sala " + $texto_codigo_sala.value + ' - ' + $texto_descricao_sala.value)
+    var responsavel_txt = document.createTextNode('Responsavel: ')
+    var data_inicio_txt = document.createTextNode('Data/hora inicio: ')
+    var data_final_txt = document.createTextNode('Data/hora final: ')
+
+    //Adiciona classes para os elementos.
+    if (nova_sala.classList) {
+        nova_sala.classList.add("sala")
+        h4_nova_sala.classList.add("tag")
+        responsavel.classList.add("tag")
+        data_inicio.classList.add("tag")
+        data_final.classList.add("tag")
     }
     else {
-        novaSala.className += "sala";
+        nova_sala.className += "sala"
     }
-    novaSala.appendChild(codigo)
-    $salas.appendChild(novaSala)
+
+    //Adciona texto aos elementos.
+    responsavel.appendChild(responsavel_txt)
+    data_inicio.appendChild(data_inicio_txt)
+    data_final.appendChild(data_final_txt)
+
+
+
+    //Adiciona filhos.
+    $salas.appendChild(nova_sala)
+    nova_sala.appendChild(h4_nova_sala)
+    h4_nova_sala.appendChild(codigo_sala)
+    nova_sala.appendChild(responsavel_txt)
+    nova_sala.appendChild(data_final)
+    nova_sala.appendChild(data_inicio)
+    h4_nova_sala.appendChild(codigo_sala)
+
+
+    alert(responsavel.classList)
 })
