3726afc9346342e9b6b7f8fc7a06020ed772c2ce
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2023-06-30 11:31:55 -0300

    Correção Favicon

--- html/agendamento.php
+++ html/agendamento.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <link rel="stylesheet" href="../css/agendamento.css">
+    <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
     <title>Agendamento</title>
 </head>
 <body>
--- html/home.php
+++ html/home.php
@@ -12,7 +12,7 @@ include "../php/conectar_banco_de_dados.php"
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/home.css">
-        <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
         <title>Home</title>
     </head>
     <body>
